5d43375b17844a3c791841aabe990d1be5c3190a
AhmetUtkuPelen <aup.aup.3548@gmail.com>
2025-03-26 22:58:44 +0300

    User Setup

--- backend/Controllers/UserControllers.ts
+++ backend/Controllers/UserControllers.ts
@@ -2,3 +2,32 @@ import { Request, Response } from 'express';
 import User from '../Models/UserModel';
 
 
+// ? GET USER PROFILE ? \\
+export const GetUserProfile = async (req: Request, res: Response) => {
+
+}
+// ? GET USER PROFILE ? \\
+
+
+
+// ? GET USER SUGGESTIONS ? \\
+export const GetUserSuggestions = async (req: Request, res: Response) => {
+
+}
+// ? GET USER SUGGESTIONS ? \\
+
+
+
+// ? FOLLOW OR UNFOLLOW A USER ? \\
+export const FollowOrUnfollowAUser = async (req: Request, res: Response) => {
+
+}
+// ? FOLLOW OR UNFOLLOW A USER ? \\
+
+
+
+// ? UPDATE USER PROFILE ? \\
+export const UpdateUserProfile = async (req: Request, res: Response) => {
+
+}
+// ? UPDATE USER PROFILE ? \\
--- backend/Routes/UserRoutes.ts
+++ backend/Routes/UserRoutes.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from 'express';
 import { ProtectedRoute } from '../Middlewares/ProtectedRoute';
+import { FollowOrUnfollowAUser, GetUserProfile, GetUserSuggestions,UpdateUserProfile } from '../Controllers/UserControllers';
 
 
 
 const UserRoutes : express.Router = express.Router();
+
+
+
+UserRoutes.get('/profile/:username',ProtectedRoute,GetUserProfile)
+UserRoutes.get('/suggestedUsers',ProtectedRoute,GetUserSuggestions)
+UserRoutes.post('/followUser/:id',ProtectedRoute,FollowOrUnfollowAUser)
+UserRoutes.post('/updateProfile',ProtectedRoute,UpdateUserProfile)
 
 
 
